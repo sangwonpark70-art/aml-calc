e4df365386e1d8d1cb95de544aab146a1eaf9663
sangwonpark70-art <sangwonpark70@gmail.com>
2026-06-22 11:15:19 +0900

    feat(pwa): V85 PWA 활성화(manifest/sw/icon) + V84 홈아이콘 교체

---
+++ v85-k3x9/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'aml-calc-v85';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// 설치: 캐시 저장
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+// 활성화: 구버전 캐시 삭제
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+// 요청: 캐시 우선, 없으면 네트워크
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response =>
+            response || fetch(event.request)
+        )
+    );
+});
