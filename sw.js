08b515fb0402ea0d8c65e2fb66ee2b190cdd53c1
sangwonpark70-art <sangwonpark70-art@users.noreply.github.com>
2026-09-18 16:22:44 +0900

    deploy: 09-17 focus fix (suppressAutoFocus) on top of 09-15 renderTree restore; sw cache 20260918

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aml-calc-v84-20260915';
+const CACHE_NAME = 'aml-calc-v84-20260918';
 const urlsToCache = [
     './',
     './index.html',
--- v85-k3x9/sw.js
+++ v85-k3x9/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aml-calc-v85-20260915';
+const CACHE_NAME = 'aml-calc-v85-20260918';
 const urlsToCache = [
     './',
     './index.html',
